0dd59086abedb319499d3556d20a7392445f1d27
“Andrew <andrewwoods88@gmail.com>
2019-08-14 11:30:02 -0400

    updated mock data

--- src/data-mock.js
+++ src/data-mock.js
@@ -163,7 +163,7 @@ export default [
       url: "https://www.udemy.com/understand-javascript/",
       free: false,
       paid: true,
-      rating: 4,
+      rating: 1,
       featured: true,
       description:
         "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
