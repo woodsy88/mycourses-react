db1a601bfd7121af205faa734537ea629bc89c0c
“Andrew <andrewwoods88@gmail.com>
2019-08-14 11:29:51 -0400

    added courses container

--- src/pages/CoursesPage.js
+++ src/pages/CoursesPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Banner from  '../components/Banner';
 import {Link} from 'react-router-dom';
+import CourseContainer from '../components/CourseContainer';
 
 const CoursesPage = () => {
   return (
@@ -11,6 +12,7 @@ const CoursesPage = () => {
           <Link to="/">Home</Link>
         </Banner>
       </Hero>
+      <CourseContainer />
     </>
   );
 };
